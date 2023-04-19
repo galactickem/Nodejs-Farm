f03f6da0a4559ab6643f097016b12fcc75bc1714
galactickem <a00827581@tec.mx>
2023-04-19 12:10:46 -0600

    Replacing data with placeholders in frontend

--- index.js
+++ index.js
@@ -29,6 +29,22 @@ const url = require("url");
 
 ///////////////////////////////////////
 // SERVER
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if (!product.organic){
+        output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic")
+    }
+
+    return output;
+}
 
 const tempOverview = fs.readFileSync(`1-node-farm/starter/templates/template-overview.html`, "utf-8");
 const tempCard = fs.readFileSync(`1-node-farm/starter/templates/template-card.html`, "utf-8");
@@ -42,7 +58,10 @@ const server = http.createServer((req, res) => {
     //Overview page
     if(pathName === "/" || pathName === "/overview"){
         res.writeHead(200, {"Content-type" : "text/html"});
-        res.end(tempOverview);
+
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join("");
+        const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+        res.end(output);
 
     //Product Page
     } else if (pathName === "/product"){
