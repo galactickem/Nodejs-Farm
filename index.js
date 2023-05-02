121cadb3fb31d34c98562814f88fdfd05095ddcd
galactickem <a00827581@tec.mx>
2023-05-02 00:00:19 -0600

    trying prettier

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(8000, "127.0.0.1", () => {
-    console.log("Listening to requests on port 8000..")
+    console.log("Listening to requests on port 8000..");
 });
 
 
